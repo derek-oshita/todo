de9af95dac4c36fc64cdf83a2c44de7be92304ed
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-01-25 17:05:23 -0800

    FlatList must use renderItem not renderTask.

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react'; 
-import {View, Text, StyleSheet, Image} from 'react-native'; 
+import {View, Text, StyleSheet, Image, FlatList} from 'react-native'; 
 import 'react-native-get-random-values'; 
 import { v4 as uuid } from 'uuid'; 
 
 import Header from './components/Header'; 
+// import ListTask from './components/ListTask'; 
 
 const App = () => {
-  const [tasks, setTasks] = useState([
+  const [items, setItems] = useState([
     {id: uuid(), text: 'Code'}, 
     {id: uuid(), text: 'Clean'}, 
     {id: uuid(), text: 'Walk Mila'}, 
@@ -15,7 +16,11 @@ const App = () => {
 
   return (
     <View style={styles.container}>
-      <Header title="Task List"></Header>
+      <Header title="Task List" />
+      <FlatList 
+      data={items} 
+      renderItem={({item}) => <Text>{item.text}</Text>}
+      />
     </View>
   )
 }
