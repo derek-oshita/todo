55b9b2785cf0bface6fa0dbdceb0b92247e3927f
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-01-25 16:10:18 -0800

    Header component created.

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react'; 
-import {View, Text} from 'react-native'; 
+import {View, Text, StyleSheet, Image} from 'react-native'; 
+
+import Header from './components/Header'; 
 
 const App = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'blue'}}>
-      <Text>Hello, World</Text>
+    <View style={styles.container}>
+      <Header></Header>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60
+  }
+})
 
 export default App; 
