59b09860807410c46280619a4f1ab0d9b21a132b
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-02-02 20:30:44 -0800

    Got a Success message but the data was blank.

--- components/GetItemsIndex.js
+++ components/GetItemsIndex.js
@@ -1,29 +1,33 @@
 import React, {Component} from 'react';
 import {View, Button} from 'react-native'; 
 
+const url = 'https://mighty-woodland-97273.herokuapp.com/api/items/'; 
+
 getData = () => {
-    return fetch('https://mighty-woodland-97273.herokuapp.com/api/items')
-    .then((response) => {
-        return response.json()
+    return fetch(url, {
+        method: 'GET', 
+        headers: {
+            'Content-Type': 'application/json'
+        },
     })
-    .then((data) => console.log(data))
+    .then((response) => {
+        return response.text()
+    })
+    .then((data) => console.log('Success:', data))
     .catch((error) => {
-        console.log(error)
+        console.log('Error:', error)
     })
 }
 
 
-
-// http://73.223.154.156:4000/api/items
-
-// issues: https://github.com/expo/expo/issues/1606
-
 /* 
-TRY DEPLOYING THE BACKEND AND THEN USING CONNECTION STRING TO MONGODB VIA CLOUD ATLAS
+random user works; https://randomuser.me/api/
 
-https://mighty-woodland-97273.herokuapp.com/api/items
+https://github.com/expo/expo/issues/1606
 
-THIS WORKED: https://enigmatic-garden-30320.herokuapp.com/api/v1/comment
+backend endpoint: https://mighty-woodland-97273.herokuapp.com/api/items
+
+quaranteam backend endpoint: https://enigmatic-garden-30320.herokuapp.com/api/v1/comment
 */
 
 class GetItemsIndex extends React.Component {
