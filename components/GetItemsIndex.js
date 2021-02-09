358aa701512d54d27cdf057b900dc8f7cb81b34b
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-02-08 19:51:43 -0800

    Got a log of something other than an error but not an array of item objects...

--- components/GetItemsIndex.js
+++ components/GetItemsIndex.js
@@ -66,7 +66,7 @@ class GetItemsIndex extends React.Component {
 
     render() {
         return (
-            <ItemsList />
+            <ItemsList items={this.state.items}/>
         )
     }
 }
--- components/ItemsList.js
+++ components/ItemsList.js
@@ -2,11 +2,11 @@ import React, {useState, Component} from 'react';
 import {View, Text, StyleSheet, Image, FlatList, Alert, AppState, Button, LogBox} from 'react-native'; 
 
 function ItemsList (props) {
-    let items = props; 
+    let items = props.items; 
 
     return (
             <View>
-            <Button title="Get Items" onPress={() => console.log(props)}>
+            <Button title="Get Items" onPress={() => console.log(props.items)}>
             
             </Button>
             </View>
